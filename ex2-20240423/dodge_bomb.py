076660cb936489ee40cb00fd92925bba6552c603
c0b23120 <c0b23120bb@edu.teu.ac.jp>
2024-04-23 17:14:22 +0900

    追加機能3 更新

--- ex2-20240423/dodge_bomb.py
+++ ex2-20240423/dodge_bomb.py
@@ -4,19 +4,24 @@ import pygame as pg
 import random
 
 
+
 WIDTH, HEIGHT = 1600, 900
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 pg_move = {
-    pg.K_UP: (0,-5),
-    pg.K_DOWN: (0,5),
-    pg.K_LEFT: (-5,0),
-    pg.K_RIGHT: (5,0)
+    pg.K_UP: (0,-7),
+    pg.K_DOWN: (0,7),
+    pg.K_LEFT: (-7,0),
+    pg.K_RIGHT: (7,0)
 }  #移動用辞書の設定
 
 
-
 def check_bound(obj_rct):  #画面外、画面内の判定
+    """
+    こうかとんRectまたは爆弾Rectの画面外判定用の関数
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：横方向判定結果、縦方向判定結果(True:画面内,Flase:画面外)
+    """
     horizontal, vertical = True, True  #横、縦が画面内
 
     if obj_rct.left < 0 or WIDTH < obj_rct.right:  #左右の画面外
@@ -26,6 +31,31 @@ def check_bound(obj_rct):  #画面外、画面内の判定
     
     return horizontal, vertical
 
+
+def GameOver():
+    """
+    ゲームオーバー画面の表示
+    ブラックアウト画面の設定→文字表示の設定→こうかとん表示の設定
+    スクリーン表示とディスプレイ更新
+    """
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #ゲームオーバー画面
+    gm_img = pg.Surface((WIDTH,HEIGHT))  #ブラックアウト
+    pg.draw.rect(gm_img,(0),(0,0,WIDTH,HEIGHT))
+    gm_img.set_alpha(150) #半透明
+    gm_rct = gm_img.get_rect()
+    gm_rct.center = WIDTH/2, HEIGHT/2
+    fonto = pg.font.Font(None,100) 
+    txt = fonto.render("Game Over",True,(0))
+    #ゲームオーバー画面のこうかとん
+    gm_kk_img = pg.transform.rotozoom(pg.image.load("fig/2.png"), 0, 2.0)
+    gm_kk_img2 = pg.transform.flip(gm_kk_img,True,False) #画像反転
+    screen.blit(gm_img,gm_rct)
+    screen.blit(txt,[WIDTH/2 - 170, HEIGHT/2 - 40])
+    screen.blit(gm_kk_img, (500,370))
+    screen.blit(gm_kk_img2, (1050,370))
+    pg.display.update()
+            
 
 
 def main():
@@ -51,9 +81,11 @@ def main():
     gm_rct.center = WIDTH/2, HEIGHT/2
     fonto = pg.font.Font(None,100) 
     txt = fonto.render("Game Over",True,(0))
-
+    #ゲームオーバー画面のこうかとん
     gm_kk_img = pg.transform.rotozoom(pg.image.load("fig/2.png"), 0, 2.0)
     gm_kk_img2 = pg.transform.flip(gm_kk_img,True,False) #画像反転
+
+    r = 0
     
 
     clock = pg.time.Clock()
@@ -66,6 +98,7 @@ def main():
             
         if kk_rct.colliderect(bd_rct): #こうかとんと爆弾の衝突時
             print("GameOver")
+            #GameOver()
 
             #ブラックアウトと文字、こうかとんの表示
             screen.blit(gm_img,gm_rct)
@@ -87,6 +120,7 @@ def main():
                 sum_mv[0] += v[0]  #上下
                 sum_mv[1] += v[1]  #左右
 
+
         #if key_lst[pg.K_UP]:
         #    sum_mv[1] -= 5
         #if key_lst[pg.K_DOWN]:
@@ -98,6 +132,17 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         bd_rct.move_ip(vx, vy)  #爆弾移動
+
+        #爆弾加速
+        vx *= 1.002
+        vy *= 1.002
+        if vx > 30:
+            vx = 30
+        if vy > 30:
+            vy = 30
+        
+        
+
 
         if check_bound(kk_rct) != (True, True):  #画面外でそれ以上進まない
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
